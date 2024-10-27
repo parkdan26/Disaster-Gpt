1687766a80e6044c0f59c7007c52d6ef0e5a10fe
MW0808 <157083716+MW0808@users.noreply.github.com>
2024-10-27 09:45:02 -0400

    Update TrainModel.py

--- data/TrainModel.py
+++ data/TrainModel.py
@@ -1,27 +1,27 @@
-import os
 import google.generativeai as genai
-from Apikey import Key
+from ApiKey import Key
 from Weather import Weather
 
+genai.configure(api_key=Key.google_k)
+
+generation_config = {
+  "temperature": 1,
+  "top_p": 0.95,
+  "top_k": 64,
+  "max_output_tokens": 8192,
+  "response_mime_type": "text/plain",
+}
+
+model = genai.GenerativeModel(
+  model_name="gemini-1.5-flash",
+)
+
+chat_session = model.start_chat(
+  history=[
+  ]
+)
+
 def ai() -> tuple:
-    genai.configure(api_key=Key.google_k)
-
-    generation_config = {
-      "temperature": 1,
-      "top_p": 0.95,
-      "top_k": 64,
-      "max_output_tokens": 8192,
-      "response_mime_type": "text/plain",
-    }
-
-    model = genai.GenerativeModel(
-      model_name="gemini-1.5-flash",
-    )
-
-    chat_session = model.start_chat(
-      history=[
-      ]
-    )
     response = chat_session.send_message(f"Create a prediction model base on weather information that I give you. "
                                          f"{prediction} gives you information in the order of temp, humidity, "
                                          f"wind_speed, precip_intensity, pressure_surface, "
@@ -32,25 +32,32 @@ def ai() -> tuple:
                                          f"and type is N/A if answer is no. Just give me the answer.")
     return response.text
 
+def to_iso(location: str):
+    response = chat_session.send_message(f"{location}, give me latitude and longitude separated by a comma "
+                                         f"Just give me the answer.")
 
-f = open("file.txt", "r")
+    return response.text
+
+
+f = open("../server/file.txt", "r")
 nextline = f.readline()
 f2 = open("Result.csv", "w")
 
 while nextline:
-    nextline = f.readline()
     if nextline.__contains__("Location"):
-        coor = nextline.split()[1]
-        coor = coor[0: len(coor)-1].split(",")
+        coor = nextline.strip().split(":")
+        iso = to_iso(coor).strip().split(",")
+        coor = [float(iso[0]), float(iso[1])]
         weather = Weather()
         weather.set(coor,"now", "nowPlus5d")
         prediction = weather.execute() #temp, humidity, wind_speed, precip_intensity, pressure_surface,
         # pressure_sea, cloud_cover, visibility
         response = ai()
-        print(response)
         if response[0].strip().lower() == 'yes':
             filename = "Result.csv"
             f2.write(str(response) + "\n")
+
+    nextline = f.readline()
 
 f2.close()
 
